2fd1d797110d2ef52a608f224fa378745cc34c25
lalee826 <71309871+lalee826@users.noreply.github.com>
2025-07-28 00:26:55 -0400

    Update 01-02.trainClassifierAndPredict.py

--- Scripts/01-02.trainClassifierAndPredict.py
+++ Scripts/01-02.trainClassifierAndPredict.py
@@ -1,34 +1,30 @@
-#%% Project description
+### Project description
 '''
 - This script trains a new neural network on DeepSVR data using only features that can be obtained for GMKF data
 - We remove liquid tumor variants and collapse the feature set to 50 extractable features from our own data set
 - The deep learning package is Keras
-
-Author: Alex Lee
-Date: 9/19/2019
 '''
 
-#%%Import packages
+### Import packages
 import pandas as pd
 import numpy as np
 import os
-import sklearn
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn import metrics
+from sklearn import preprocessing as preprocessing
+from sklearn import metrics as metrics
 import matplotlib.pyplot as plt
 from itertools import cycle
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 from tensorflow.keras.regularizers import l2
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 
 workdir = '/rocker-build/gmkf_nbl_somatic/'
 
-#%% Import and prepare training data
+### Import and prepare training data
 trainSet = workdir + 'Data/deepSVR_traindata.pkl'
 train = pd.read_pickle(trainSet)
 #remove AML samples
@@ -79,130 +75,38 @@ train = train.rename(columns={'normal_ref_avg_num_mismaches_as_fraction':'normal
                       'tumor_var_avg_num_mismaches_as_fraction':'tumor_var_avg_num_mismatches_as_fraction'})
 trainCols = train.columns.values
 
-#import our own data
-nblData = workdir + 'Data/gmkf_matched_features.pkl'
-tmbdata = pd.read_pickle(nblData)
-tmbdata = tmbdata.rename(columns={'normal_ref_avg_num_mismaches_as_fraction':'normal_ref_avg_num_mismatches_as_fraction',
-                      'tumor_ref_avg_num_mismaches_as_fraction':'tumor_ref_avg_num_mismatches_as_fraction',
-                      'normal_var_avg_num_mismaches_as_fraction':'normal_var_avg_num_mismatches_as_fraction',
-                      'tumor_var_avg_num_mismaches_as_fraction':'tumor_var_avg_num_mismatches_as_fraction'})
-tmbdata = tmbdata[trainCols[1:].tolist()]
-
-mm_scaler = preprocessing.MinMaxScaler()
-data_scaled = mm_scaler.fit_transform(tmbdata)
-data_scaled = pd.DataFrame(data_scaled, index=tmbdata.index,
-                      columns=tmbdata.columns)
-
-trainss = train.iloc[:100,:]
-datass = data_scaled.iloc[:100,:]
-
-train.shape #(32308, 59)
-data_scaled.shape #(12481, 58)
-
-#%% Split train data into features and labels
-train_y = pd.get_dummies(train.call).astype(float).values #get labels
+### Split train data into features and labels
+x_train = train.drop(['call'],axis=1).astype(float).values #get features
 '''
 0 - ambiguous
 1 - fail
 2 - somatic
 '''
-train_x = train.drop(['call'],axis=1).astype(float).values #get features
-#fix random seed for reproducibility
-seed = 999
-np.random.seed(seed)
+#min max scale train data
+mm_scaler = preprocessing.MinMaxScaler()
+x_train = mm_scaler.fit_transform(x_train)
 
-#%% train our neural network
-#we only want simple feed-forward layers (type Dense)
-#configure the model based on the data
-n_input, n_classes = train_x.shape[1], train_y.shape[1]
-#define model
-model = keras.models.Sequential() #connected feed forward
-#add first layer 50 nodes wide
-model.add(Dense(50, input_dim=n_input, activation='relu', kernel_initializer='normal',kernel_regularizer=keras.regularizers.l2(0.001)))
-#add three layers 25 nodes wide each
-for i in range(3): #add layers
-    model.add(Dense(25,activation='relu',kernel_regularizer=l2(0.001)))
-#add 3 layers 6 nodes wide each
-#for i in range(10): #add layers
-#    model.add(Dense(6,activation='sigmoid',kernel_regularizer=l2(0.001)))
-#add a final softmax layer
-model.add(Dense(n_classes,kernel_initializer='normal',activation='softmax'))
-#determine how to compile model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit model on train set
-fitmodel = model.fit(train_x,train_y, epochs=100, verbose=1)#, batch_size=2000)
-
-#%% Make predictions
-#evaluate model on test set
-data_pred_labels = model.predict(data_scaled.iloc[:,:58], verbose=1)
-predicted = np.array([list(a).index(max(list(a))) for a in list(data_pred_labels)])
-#look at prediction counts
-print(np.bincount(predicted))
-'''
-counts are shown as ambiguous, fail, and somatic
-'''
-
-#add classifier predictions to each variant
-data_scaled['classification'] = predicted
+#one-hot encode labels
+y_train = pd.get_dummies(train.call).astype(int).values
 
 #### CROSS VALIDATION OF BEST MODEL PARAMETERS ####
-#%% Split into train/test sets, and then cross-validation sets
-Y = pd.get_dummies(train.call).astype(float).values #get labels
-X = train.drop(['call'],axis=1).astype(float).values #get features
-#fix random seed for reproducibility
-seed = 13
-np.random.seed(seed)
-#split into train/test
-x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(X,Y,test_size=0.15,random_state=seed)
+#set cross-val params
+seed = 999
+kfold = KFold(n_splits=5,shuffle=True,random_state=np.random.seed(seed))
 
-#%% train neural network and evaluate performance
-#usesimple feed-forward layers (type Dense)
+#test parameters of nn structure
+nnodes = [25,50]
+nlayers = [2,3]
+activations = ['relu','sigmoid','tanh']
 
-#configure the model based on the data
-n_input, n_classes = x_train.shape[1], y_test.shape[1]
-#define model
-model = keras.models.Sequential() #connected feed forward
-#add first layer 50 nodes wide
-model.add(Dense(50, input_dim=n_input, activation='relu', kernel_initializer='he_uniform',kernel_regularizer=keras.regularizers.l2(0.001)))
-#add three layers 25 nodes wide each
-for i in range(3): #add layers
-    model.add(Dense(25,activation='relu',kernel_regularizer=l2(0.001)))
-#add 3 layers 6 nodes wide each
-#for i in range(10): #add layers
-#    model.add(Dense(6,activation='sigmoid',kernel_regularizer=l2(0.001)))
-#add a final softmax layer
-model.add(Dense(n_classes,kernel_initializer='random_uniform',activation='softmax'))
-#determine how to compile model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit model on train set
-fitmodel = model.fit(x_train, y_train, epochs=100, verbose=1)
-#evaluate model on test set
-_, test_acc = model.evaluate(x_test, y_test, verbose=1)
-pred_labels = model.predict(x_test, verbose=1)
-#%%classify test set
-predicted = np.array([list(a).index(max(list(a))) for a in list(pred_labels)])
-#score the predictions
-label_binarizer = preprocessing.LabelBinarizer()
-label_binarizer.fit(range(max(predicted)+1))
-predicted_transformed = label_binarizer.transform(predicted)
+#store cross-validation metrics
+nnCV = {}
 
-#%%check results
-print('Model accuracy:\t' +  
-      str(sklearn.metrics.accuracy_score(y_test, predicted_transformed)) + 
-      '\nModel Performance report\n' + 
-      str(sklearn.metrics.classification_report(y_test, predicted_transformed)))
-
-#%% set cross-val params
-kfold = sklearn.model_selection.KFold(n_splits=10,shuffle=True,random_state=seed)
-nnodes, nlayers, activations = [25,50], [1,2,3], ['relu','sigmoid','tanh']
-nnodes, nlayers, activations = [50], [3], ['relu']
-#histories = {}
-#test_results = {}
-cv_scores = {}
-probabilities_d = {}
 #change y train array from one-hot encoded to single dimension
 y_train_1d = np.argmax(y_train, axis=1)
-input_dims = x_train.shape[1]
+
+#get feature number
+nFeatures = x_train.shape[1]
 
 for act in activations:
     for layer in nlayers:
@@ -210,80 +114,158 @@ for act in activations:
             print('activation function: {}'.format(act))
             print('layer number: {}'.format(layer))
             print('nodes per layer: {}'.format(nnum))
-            #set training params
-            def buildModelCV():
+            
+            #name the parameter set
+            name = 'activation: ' + act + ', num_layers: ' + str(layer) + ', num_nodes: ' + str(nnum)
+            
+            #build the model using training params
+            def buildModelCV(actf,nlayer,nodes):
                 model = keras.models.Sequential()
-                model.add(Dense(50,input_dim=input_dims,kernel_initializer=keras.initializers.RandomUniform(minval=-0.05,maxval=0.05,seed=seed),
-                            activation='relu',kernel_regularizer=keras.regularizers.l2(0.001))) #input and output of initial layer are both matrices of dimensions [*,50]
-                for i in range(layer): #add layers
-                    model.add(Dense(nnum,activation=act,kernel_regularizer=l2(0.001)))
+                model.add(Dense(50,input_dim=nFeatures,kernel_initializer=keras.initializers.RandomUniform(minval=-0.05,maxval=0.05,seed=seed),
+                            activation=actf,kernel_regularizer=keras.regularizers.l2(0.001))) #input and output of initial layer are both matrices of dimensions [*,50]
+                for i in range(nlayer): #add layers
+                    model.add(Dense(nodes,activation=actf,kernel_regularizer=l2(0.001)))
                 model.add(Dense(3,kernel_initializer='random_uniform',activation='softmax')) #add a final softmax layer corresponding to three labels
-                model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) #compile the model
+                model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy']) #compile the model
                 return(model)
-            nnEstimator = KerasClassifier(build_fn=buildModelCV, epochs=100, batch_size=1500, verbose=0)
-            probabilities = cross_val_predict(nnEstimator, x_train, y_train_1d, cv=kfold, method='predict_proba')
-            #score the predictions
-            predicted = np.array([list(a).index(max(list(a))) for a in list(probabilities)])
-            label_binarizer = preprocessing.LabelBinarizer()
-            label_binarizer.fit(range(max(predicted)+1))
-            predicted_transformed = label_binarizer.transform(predicted)
-            cv_acc = sklearn.metrics.accuracy_score(y_train, predicted_transformed)
-            probabilities_d.update({act + '_' + str(nnum) + '_' + str(layer) + 'layers':probabilities})
-            cv_scores.update({act + '_' + str(nnum) + '_' + str(layer) + 'layers':cv_acc})
-            print('number of layers = %d; number of nodes = %d: %.3f' % (layer, nnum, cv_acc))
+            
+            #store cv scores
+            acc = []
+            prec = {0:[],1:[],2:[]}
+            recall = {0:[],1:[],2:[]}
+            f1 = {0:[],1:[],2:[]}
+            
+            #test parameters in cross-validation
+            for trainix,testix in kfold.split(x_train):
+                
+                #split cross val data
+                xtrain,ytrain = x_train[trainix,:], y_train_1d[trainix]
+                xtest,ytest = x_train[testix,:], y_train_1d[testix]           
+                
+                #build nn with specific params
+                nnModel = KerasClassifier(build_fn=lambda: buildModelCV(act,layer,nnum), epochs=100, batch_size=1500, verbose=0)
+                
+                #fit the model of cross val train data
+                fitmodel = nnModel.fit(xtrain, ytrain)
+                      
+                #evaluate model on test set
+                predicted = nnModel.predict(xtest, verbose=0)
+                      
+                #classify test set
+                #predicted = np.array([list(a).index(max(list(a))) for a in list(pred_labels)])
+                
+                #score the predictions
+                label_binarizer = preprocessing.LabelBinarizer()
+                label_binarizer.fit(range(max(predicted)+1))
+                ypredOneHot = label_binarizer.transform(predicted)
+                ytestOneHot = label_binarizer.transform(ytest)
+                
+                #evaluate performance
+                ac = round(metrics.accuracy_score(ytestOneHot,ypredOneHot),2)
+                pr = metrics.precision_score(ytestOneHot,ypredOneHot,average=None).round(2)
+                re = metrics.recall_score(ytestOneHot,ypredOneHot,average=None).round(2)
+                f1s = metrics.f1_score(ytestOneHot,ypredOneHot,average=None).round(2)
+        
+                
+                #save score for this kfold test
+                acc.append(ac)
+                prec[0].append(pr[0])
+                prec[1].append(pr[1])
+                prec[2].append(pr[2])
+                recall[0].append(re[0])
+                recall[1].append(re[1])
+                recall[2].append(re[2])
+                f1[0].append(f1s[0])
+                f1[1].append(f1s[1])
+                f1[2].append(f1s[2])
+                
+            #save parameter metrics
+            nnCV[name] = {'name':name,
+            'avgAccuracy':np.mean(acc).round(2),
+            'avgPrecision_f':np.mean(prec[0]).round(2),
+            'avgPrecision_a':np.mean(prec[1]).round(2),
+            'avgPrecision_s':np.mean(prec[2]).round(2),
+            'avgRecall_f':np.mean(recall[0]).round(2),
+            'avgRecall_a':np.mean(recall[1]).round(2),
+            'avgRecall_s':np.mean(recall[2]).round(2),
+            'avgF1_f':np.mean(f1[0]).round(2),
+            'avgF1_a':np.mean(f1[1]).round(2),
+            'avgF1_s':np.mean(f1[2]).round(2)}  
+                
+### train our neural network with best parameters
+'''
+best parameters in cross-validation:
+ 'activation: relu, num_layers: 3, num_nodes: 50': {'avgAccuracy': 0.85,
+  'avgF1_a': 0.9,
+  'avgF1_f': 0.73,
+  'avgF1_s': 0.88,
+  'avgPrecision_a': 0.91,
+  'avgPrecision_f': 0.78,
+  'avgPrecision_s': 0.86,
+  'avgRecall_a': 0.9,
+  'avgRecall_f': 0.69,
+  'avgRecall_s': 0.91,
+'''
+#configure the model based on the data
+n_input, n_classes = x_train.shape[1], y_train.shape[1]
+#define model
+model = keras.models.Sequential() #connected feed forward
+#add first layer 50 nodes wide
+model.add(Dense(50, input_dim=n_input, activation='relu', kernel_initializer='normal',kernel_regularizer=keras.regularizers.l2(0.001)))
+#add three layers 25 nodes wide each
+for i in range(3): #add layers
+    model.add(Dense(50,activation='relu',kernel_regularizer=l2(0.001)))
+#add 3 layers 6 nodes wide each
+#for i in range(10): #add layers
+#    model.add(Dense(6,activation='sigmoid',kernel_regularizer=l2(0.001)))
+#add a final softmax layer
+model.add(Dense(n_classes,kernel_initializer='normal',activation='softmax'))
+#determine how to compile model
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model on train set
+fitmodel = model.fit(x_train,y_train_1d, epochs=100, verbose=0)#, batch_size=2000)
 
-#%%top performance is in relu/50/3
-probabilities = probabilities_d['relu_50_3layers']
-predicted = np.array([list(a).index(max(list(a))) for a in list(probabilities)])
-#score the predictions
-label_binarizer = preprocessing.LabelBinarizer()
-label_binarizer.fit(range(max(predicted)+1))
-predicted_transformed = label_binarizer.transform(predicted)
+### import our own data
+consensusVarsFile = workdir + 'Data/coding_mutations_annotated.tsv'
+uniqueVarsFile = workdir + 'Data/unique_coding_mutations_full.tsv'
+bamReadCountsFiles = workdir + 'Data/variants_bamrc_analyzed.tsv'
 
-#%% evaluate performance of best model
-print('Cross validation accuracy:')
-print('\t', sklearn.metrics.accuracy_score(y_train, predicted_transformed))
-print('\nCross validation classification report\n')
-print(sklearn.metrics.classification_report(y_train, predicted_transformed))
+consensusVars = pd.read_table(consensusVarsFile,encoding="latin1")
+uniqueVars = pd.read_table(uniqueVarsFile)
+brcData = pd.read_table(bamReadCountsFiles)
 
-#ROC for three classes using RELU/3/50
-def create_roc_curve(Y, probabilities, class_lookup, title, ax):
-    '''Create ROC curve to compare multiclass model performance.
+#merge consensus variants and unique variants and grab bam-readcount output
+nblData = pd.concat([brcData.merge(consensusVars[['mutid']],on='mutid',how='inner'),
+                   brcData.merge(uniqueVars[['mutid']],on='mutid',how='inner')]).reset_index(drop=True)
+#set mutid as index
+nblData.set_index('mutid',inplace=True)
+#rename columns to match train data
+nblData = nblData.rename({'normal_ref_avg_num_mismaches_as_fraction':'normal_ref_avg_num_mismatches_as_fraction',
+              'tumor_ref_avg_num_mismaches_as_fraction':'tumor_ref_avg_num_mismatches_as_fraction',
+              'normal_var_avg_num_mismaches_as_fraction':'normal_var_avg_num_mismatches_as_fraction',
+              'tumor_var_avg_num_mismaches_as_fraction':'tumor_var_avg_num_mismatches_as_fraction'},axis=1)
 
-    Parameters:
-        Y (numpy.array): Truth labels
-        probabilities (numpy.array): Output of model for each class
-        class_lookup (dict): lookup hash of truth labels
-        title (str): Plot title
-    '''
-    n_classes = Y.shape[1]
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    ax.set_title(title)
-    if n_classes == 3:
-        colors = cycle(['orange', 'red', 'black'])
-    else:
-        colors = cycle(['orange', 'red', 'aqua', 'black'])
-    for i, color in zip(range(n_classes), colors):
-        fpr[i], tpr[i], _ = metrics.roc_curve(Y[:, i], probabilities[:, i])
-        roc_auc[i] = metrics.auc(fpr[i], tpr[i])
-        ax.plot(fpr[i], tpr[i], color=color,
-                label='ROC curve of class {0} (area = {1:0.2f})'.format(
-                    class_lookup[i], roc_auc[i]))
-    ax.plot([0, 1], [0, 1], 'k--')
-    ax.set_xlim([0, 1.0])
-    ax.set_ylim([0.0, 1.0])
-    ax.set_xlabel('False Positive Rate')
-    ax.set_ylabel('True Positive Rate')
-    ax.legend(loc="lower right")
-    plt.show()
+#order columns to match train data
+matchCols = np.delete(trainCols,np.where(trainCols=='call')[0])
+nblData = nblData[matchCols]
+#min max scale the data
+mm_scaler = preprocessing.MinMaxScaler()
+data_scaled = mm_scaler.fit_transform(nblData)
+data_scaled = pd.DataFrame(data_scaled, index=nblData.index,
+                      columns=nblData.columns)
 
-class_lookup = {0: 'Ambiguous', 1: 'Fail', 2: 'Somatic'}
-fig, ax = plt.subplots()
-create_roc_curve(y_train, probabilities, class_lookup, 'Three Class Reciever '
-                'Operating Characteristic Curve',ax)
+#make predictions using trained model
+predLabelsNBL = model.predict(data_scaled, verbose=0)
 
+#use max softmax probability to assign labels
+predicted = np.array([list(a).index(max(list(a))) for a in list(predLabelsNBL)])
+#look at prediction counts
+print(np.bincount(predicted))
 
-#output the final count of labels on our test data set
-predCounts = data_scaled.groupby('classification').size()
+#add classifier predictions to each variant
+nblData['prediction'] = predicted
+nblData['prediction'].replace({0:'ambiguous',1:'fail',2:'somatic'},inplace=True)
+
+#join back to consensus and unique variant mutations files
+consensusVars = consensusVars.merge(nblData,on='mutid',how='left')
+uniqueVars = uniqueVars.merge(nblData,on='mutid',how='left')
